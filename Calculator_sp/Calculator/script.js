2363ea0e1cca53feb104d0dc24af8cb868e59ebe
Sajeshspsp <113038153+Sajeshspsp@users.noreply.github.com>
2023-09-04 23:15:17 +0530

    Add files via upload

---
+++ Calculator_sp/Calculator/script.js
@@ -0,0 +1,26 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+//Define function 
+const calculate = (btnValue) => {
+  display.focus();
+  if (btnValue === "=" && output !== "") {
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    output = output.toString().slice(0, -1);
+  } else {
+  
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+};
+
+buttons.forEach((button) => {
+  //Button click listener
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
